81c4c406a1a87a8c0e802dff9eae2762981eb8d7
janindujm <janindujm@gmail.com>
2025-05-01 07:39:33 +0530

    Add post Lists

---
+++ blog/client/src/PostList.js
@@ -0,0 +1,43 @@
+import React,{useState , useEffect} from "react";
+import axios from "axios";
+
+
+
+export default () => {
+const [posts, setPosts] = useState({});
+
+const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:3000/posts");
+
+    setPosts(res.data);
+};
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    console.log(posts);
+    const renderedPosts = Object.values(posts).map(post => {
+        return (
+        <div 
+            className = "card" 
+            key={post.id} 
+            style={{width: '30%', marginBottom: '20px'}}>
+
+            <div className="card-body">
+                <h3>{post.title}</h3>
+               <button className="btn btn-primary">View Comments</button>
+
+            </div>
+        </div>
+        );
+
+    });
+
+    return (
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {renderedPosts}
+
+        </div>
+    );
+};
